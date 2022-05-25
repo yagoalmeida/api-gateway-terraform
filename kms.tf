419e1a92e2a466c80aa0b452d47218f436051624
Yago Almeida <almeida_yago@hotmail.com>
2022-05-25 01:00:58 -0300

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,19 +1,25 @@
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
 
-data "aws_partition" "current" {}
+}
 
-data "aws_region" "current" {}
+data "aws_partition" "current" {
+
+}
+
+data "aws_region" "current" {
+
+}
 
 data "aws_iam_policy_document" "cloudwatch" {
   policy_id = "key-policy-cloudwatch"
   statement {
-    sid     = "Enable IAM User Permissions"
+    sid = "Enable IAM User Permissions"
     actions = [
       "kms:*",
     ]
     effect = "Allow"
     principals {
-      type        = "AWS"
+      type = "AWS"
       identifiers = [
         format(
           "arn:%s:iam::%s:root",
@@ -25,7 +31,7 @@ data "aws_iam_policy_document" "cloudwatch" {
     resources = ["*"]
   }
   statement {
-    sid     = "AllowCloudWatchLogs"
+    sid = "AllowCloudWatchLogs"
     actions = [
       "kms:Encrypt*",
       "kms:Decrypt*",
@@ -35,7 +41,7 @@ data "aws_iam_policy_document" "cloudwatch" {
     ]
     effect = "Allow"
     principals {
-      type        = "Service"
+      type = "Service"
       identifiers = [
         format(
           "logs.%s.amazonaws.com",
